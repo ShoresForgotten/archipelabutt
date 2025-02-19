3df6c7de2b1675ecd903abbe62d7afa6e0900422
ShoresForgotten <shores@shoresforgotten.xyz>
2025-02-19 17:15:59 +0100

    Progress made on connection logic

---
+++ lib/archipelago_text_client.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import 'archipelago/protocol_types.dart';
+import 'archipelago/server_to_client.dart';
+
+class ArchipelagoLogWidget extends StatelessWidget {}
